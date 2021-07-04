d3f785ab7b825e917d1fc3f06e45aaf4fcf97eb9
reset54 <reset54@yandex.ru>
2021-07-04 14:44:22 +0300

    add Circle.py file in dir OOP

---
+++ Object Oriented Proggraming/Circle.py
@@ -0,0 +1,29 @@
+import math
+
+
+class Circle:
+    """
+    Окружность (Circle)
+    Площадь (area) = Pi * R**2
+    perimeter = 2 * Pi * R
+    """
+    def __init__(self, radius: float):
+        self.radius = radius
+
+    def get_area(self) -> float:
+        """Площадь"""
+        return math.pi * self.radius
+
+    def get_length(self) -> float:
+        """Длина (периметр)"""
+        return (2 * math.pi * self.radius)
+
+    def get_diameter(self) -> float:
+        """Диаметр"""
+        return 2 * self.radius
+
+first_circle = Circle(radius=10)
+second_circle = Circle(radius=30)
+
+print(first_circle.get_area())
+print(second_circle.get_diameter()) 
