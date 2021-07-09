667b75241207c325d74bc2ff76c9a37430513232
reset54 <reset54@yandex.ru>
2021-07-09 11:25:22 +0300

    addition decide and Unittests (asserts Equal) 09.07.2021

--- Algorithms/prime_numbers.py
+++ Algorithms/prime_numbers.py
@@ -13,58 +13,56 @@ def is_prime_number(number: int) -> bool:
         check = True
         return check
     
-    for i in range(2, number+1):
+    for i in range(2, number):
         if number % i == 0:
-            print(i, number % i)
+            # print(i, number % i)      # -> test
             return(False) 
             break
         else:
-            print(i, number % i)
+            # print(i, number % i)      # -> test
             check = "True"
     return check
 
 def get_next_prime_number(number: int) -> int:
     """Returns next primer number after `number`
-    get_next_prime_number - вернёт следующее простое число, большее, чем введённое 
+    get_next_prime_number - получает целое число и везвращает следующее простое число, большее, чем введённое 
     (независимо простое оно или нет)
     """
-    check = (is_prime_number(number))
     number += 1
+    check = (is_prime_number(number))
     while(True):
         if(check == False):
-            print(number)
-            number += 1    
+            # print(number)             # -> test
+            number += 1
+            check = (is_prime_number(number))    
         else:
-            # number += 1 
             print("end cycle")
             break
-    print(number)
+    return number
 
 
 
 
-#class Test_is_prime_number(unittest.TestCase):
+class Test_is_prime_number(unittest.TestCase):
     
-#   def test_is_prime_number(self): 
-#        self.assertEqual(is_prime_number(30), False)
-#        self.assertEqual(is_prime_number(77), False)
-#        self.assertEqual(is_prime_number(7), True)
+    def test_is_prime_number(self): 
+        self.assertEqual(is_prime_number(30), False)
+        self.assertEqual(is_prime_number(77), False)
+        self.assertEqual(is_prime_number(7), "True")
 
-#    def test_get_next_prime_number(self):
-        #self.assertEqual(get_next_prime_number(30), 31)
-        #self.assertEqual(get_next_prime_number(77), 79)
-        #self.assertEqual(get_next_prime_number(7), 11)
-        #self.assertEqual(get_next_prime_number(11), 13)
-        #self.assertEqual(get_next_prime_number(115), 127)
-        #self.assertEqual(get_next_prime_number(1975), 1979)
-#        pass
+    def test_get_next_prime_number(self):
+        print(self.assertEqual(get_next_prime_number(30), 31))
+        self.assertEqual(get_next_prime_number(77), 79)
+        self.assertEqual(get_next_prime_number(7), 11)
+        self.assertEqual(get_next_prime_number(11), 13)
+        self.assertEqual(get_next_prime_number(115), 127)
+        self.assertEqual(get_next_prime_number(1975), 1979) 
 
-#if __name__ == '__main__':
-#    unittest.main()
+if __name__ == '__main__':
+    unittest.main()
 
 
 
 
 print(f"\nCheck on prime number = {is_prime_number(num)}")
-
 print(f"\nNext prime number = {get_next_prime_number(num)}")
