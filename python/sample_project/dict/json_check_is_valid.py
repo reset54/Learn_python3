5b185baf37b31f78de1a7ad310a9674a585496c9
= <>
2021-07-25 07:49:36 +0000

    modify file json_check_is_valid.py 25.07.2021

--- python/sample_project/dict/json_check_is_valid.py
+++ python/sample_project/dict/json_check_is_valid.py
@@ -2,11 +2,21 @@ from pprint import pprint
 import json
 
 
-try:
-	sample_input = [{"phone": "01", "name": "John"}, "2", {"phone": "033", "name": "Vasya"}, "4", {"phone": "777", "name": "Daniel"}]
-	pprint(sample_input)
+def is_valide_JSON(json_Data):
+    try:
+        pprint(json.loads(json_Data))
+    except ValueError as err:
+        print("Невалидный json")
 
-	new_json = json.dumps(sample_input, indent=2)
-	pprint(new_json)
-except Exception:
-	print("Невалидный json")
+InvalidJsonData = """{"name": "jane doe", "salary": 9000, "email": "jane.doe@pynative.com",}"""
+
+validJsonData = """{"name": "jane doe", "salary": 9000, "email": "jane.doe@pynative.com"}"""
+
+validJsonData2 = """[{"phone": "01", "name": "John"}, "2", {"phone": "033", "name": "Vasya"}, "4", {"phone": "777", "name": "Daniel"}]"""
+
+is_valide_JSON(InvalidJsonData)
+print()
+is_valide_JSON(validJsonData)
+print()
+is_valide_JSON(validJsonData2)
+print()
