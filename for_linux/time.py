410277b6c424a1e2fb4dcdd2a217444e110d36a1
= <>
2021-07-25 10:44:49 +0000

    updates files 25.07.2021

---
+++ for_linux/time.py
@@ -0,0 +1,9 @@
+import datetime
+import pytz
+
+timezone = pytz.timezone("Europe/Moscow")
+now = timezone.localize(datetime.datetime.utcnow())
+
+# append row in file "now.txt"
+with open("./now.txt", "a") as f:
+    f.write(f"Now: {now.hour:02d}:{now.minute:02d}:{now.second:02d}\n")
