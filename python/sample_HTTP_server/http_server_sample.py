d99230810ae6f14f218165fc7e7ee0ee8699e37a
= <>
2021-07-27 16:24:08 +0000

    update server +response

--- python/sample_HTTP_server/http_server_sample.py
+++ python/sample_HTTP_server/http_server_sample.py
@@ -13,15 +13,18 @@ server.setsockopt(
 )
 
 server.listen(10)
-client_socket, address = server.accept()
-received_data = client_socket.recv(1024).decode("utf-8")
+while True:
+    client_socket, address = server.accept()
+    received_data = client_socket.recv(1024).decode("utf-8")
 
-print("Получили данные по соккету", received_data)
+    print("Получили данные по соккету", received_data)
 
-path  = received_data.split(" ")[1]
-response = f"HTTP/1.1 200 OK\nContent-Type: text/html; charset=utf-8\n\n"\
+    path  = received_data.split(" ")[1]
+    response = f"HTTP/1.1 200 OK\nContent-Type: text/html; charset=utf-8\n\n"\
            f"Привет! <br/> path: {path}"
-client_socket.send(response.encode("utf-8"))
-client_socket.shutdown(socket.SHUT_RDWR)
+    client_socket.send(response.encode("utf-8"))
+    client_socket.shutdown(socket.SHUT_RDWR)
+
+server.shutdown(socket.SHUT_RDWR)
 server.close()
 
