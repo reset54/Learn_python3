d8fe5b1831ba7344b5c459b146ff0ad50bf7874c
Андрей Azanov <40237958+reset54@users.noreply.github.com>
2021-07-27 16:32:37 +0000

    + KeyboardInterrupt consider

--- python/sample_HTTP_server/http_server_sample.py
+++ python/sample_HTTP_server/http_server_sample.py
@@ -11,20 +11,21 @@ server.setsockopt(
     socket.SO_REUSEADDR, 
     1
 )
-
 server.listen(10)
-while True:
-    client_socket, address = server.accept()
-    received_data = client_socket.recv(1024).decode("utf-8")
 
-    print("Получили данные по соккету", received_data)
+try:
+    while True:
+        client_socket, address = server.accept()
+        received_data = client_socket.recv(1024).decode("utf-8")
 
-    path  = received_data.split(" ")[1]
-    response = f"HTTP/1.1 200 OK\nContent-Type: text/html; charset=utf-8\n\n"\
-           f"Привет! <br/> path: {path}"
-    client_socket.send(response.encode("utf-8"))
-    client_socket.shutdown(socket.SHUT_RDWR)
+        print("Получили данные по соккету", received_data)
 
-server.shutdown(socket.SHUT_RDWR)
-server.close()
+        path  = received_data.split(" ")[1]
+        response = f"HTTP/1.1 200 OK\nContent-Type: text/html; charset=utf-8\n\n"\
+            f"Привет! <br/> path: {path}"
+        client_socket.send(response.encode("utf-8"))
+        client_socket.shutdown(socket.SHUT_RDWR)
+except KeyboardInterrupt:
+    server.shutdown(socket.SHUT_RDWR)
+    server.close()
 
