bab715ea7d16e2620ae18655b2db841155b49c9d
Andrew <40237958+reset54@users.noreply.github.com>
2021-10-13 21:24:50 +0300

    Update Circle.py

--- Object Oriented Proggraming/Circle.py
+++ Object Oriented Proggraming/Circle.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import math
 
 
@@ -28,34 +27,3 @@ second_circle = Circle(radius=30)
 
 print(first_circle.get_area())
 print(second_circle.get_diameter()) 
-=======
-import math
-
-
-class Circle:
-    """
-    Окружность (Circle)
-    Площадь (area) = Pi * R**2
-    perimeter = 2 * Pi * R
-    """
-    def __init__(self, radius: float):
-        self.radius = radius
-
-    def get_area(self) -> float:
-        """Площадь"""
-        return math.pi * self.radius
-
-    def get_length(self) -> float:
-        """Длина (периметр)"""
-        return (2 * math.pi * self.radius)
-
-    def get_diameter(self) -> int:
-        """Диаметр"""
-        return 2 * self.radius
-
-# first_circle = Circle()
-# second_circle = Circle(radius=30)
-
-# print(first_circle.get_area())
-# print(second_circle.get_diameter())
->>>>>>> refs/remotes/origin/readme
