19cca22d98c241a11e517427567241bc2f46dd2d
Andrew <40237958+reset54@users.noreply.github.com>
2021-10-13 16:52:37 +0300

    Update prime_numbers.py

--- Algorithms/prime_numbers.py
+++ Algorithms/prime_numbers.py
@@ -1,43 +1,7 @@
-num = int(input())
-
-print("\nФункция \nis_prime_number - \n    проверка переданного числа на простоту;\nget_next_prime_number - \n    вернёт следующее простое число, большее, чем введённое \n    (независимо простое оно или нет)")
-
-def is_prime_number(number: int) -> bool:
-    """Returns True, if `number` is prime, otherwise returns False"""
-    check = 0
-    for i in range(2, number+1):
-        
-        if number % i == 0:
-            #print(i, number % i)
-            return(False)
-            break
-        else:
-            #print(i, number % i)
-            check = "True"
-    return check
-
-def get_next_prime_number(number: int) -> int:
-    """Returns next primer number after `number`"""
-    check = (is_prime_number(number))
-    number += 1
-    while(True):
-        if(check == False):
-            # print(number)
-            number += 1    
-        else:
-            # number += 1 
-            print("end cycle")
-            break
-    print(number)
-
-
-print(f"\nCheck on prime number = {is_prime_number(num)}")
-
-=======
 import unittest
 
+num = int(input())
 
-num = int(input()) # -> int
 
 def is_prime_number(number: int) -> bool:
     """Returns True, if `number` is prime, otherwise returns False
@@ -59,6 +23,7 @@ def is_prime_number(number: int) -> bool:
             check = "True"
     return check
 
+
 def get_next_prime_number(number: int) -> int:
     """Returns next primer number after `number`
     get_next_prime_number - получает целое число и везвращает следующее простое число, большее, чем введённое 
@@ -68,7 +33,7 @@ def get_next_prime_number(number: int) -> int:
     check = (is_prime_number(number))
     while(True):
         if(check == False):
-            # print(number)             # -> test
+            # print(number)                                         
             number += 1
             check = (is_prime_number(number))    
         else:
@@ -77,9 +42,7 @@ def get_next_prime_number(number: int) -> int:
     return number
 
 
-
-
-class Test_is_prime_number(unittest.TestCase):
+class Test_is_prime_number(unittest.TestCase):                      # extends from unittest.TestCase
     
     def test_is_prime_number(self): 
         self.assertEqual(is_prime_number(30), False)
@@ -97,9 +60,5 @@ class Test_is_prime_number(unittest.TestCase):
 if __name__ == '__main__':
     unittest.main()
 
-
-
-
 print(f"\nCheck on prime number = {is_prime_number(num)}")
->>>>>>> refs/remotes/origin/readme
 print(f"\nNext prime number = {get_next_prime_number(num)}")
