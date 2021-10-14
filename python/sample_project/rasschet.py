6724c15ebc73995131b983ea31bc5232b1a597e1
Andrew <40237958+reset54@users.noreply.github.com>
2021-10-14 11:14:00 +0300

    Update rasschet.py

--- python/sample_project/rasschet.py
+++ python/sample_project/rasschet.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 number = int(input("Введите сумму/мес для сдачи квартиры в аренду : ")) 
 long_time = int(input("Введите срок (месяцев) аренды : "))
 cost_based = int(input("Введите сумму для сравнения : "))
@@ -7,14 +6,4 @@ def return_cost_percent(number: int, long_time: int, cost_based: float) -> float
     result = (long_time * number)/cost_based * 100
     return result
 
-=======
-number = int(input("Введите сумму/мес для сдачи квартиры в аренду : ")) 
-long_time = int(input("Введите срок (месяцев) аренды : "))
-cost_based = int(input("Введите сумму для сравнения : "))
-
-def return_cost_percent(number: int, long_time: int, cost_based: float) -> float:
-    result = (long_time * number)/cost_based * 100
-    return result
-
->>>>>>> refs/remotes/origin/readme
 print(return_cost_percent(number, long_time, cost_based))
