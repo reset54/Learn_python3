739d02f7e1d7db0e88e4b2385ecaf283120200d7
Andrew <40237958+reset54@users.noreply.github.com>
2021-10-14 11:09:12 +0300

    Update check_more_hundred.py

--- python/sample_project/check_more_hundred.py
+++ python/sample_project/check_more_hundred.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 string = input()
 numbers = string.split(" ")
 one = int(numbers[0])
@@ -14,21 +13,4 @@ if one > 100 and two > 100:
         print(max)
 else:
     print(two + 112)
-=======
-string = input()
-numbers = string.split(" ")
-one = int(numbers[0])
-two = int(numbers[1])
-
-max = one
-
-if one > 100 and two > 100:
-    if two > one:
-        max = two
-        print(max)
-    else:
-        print(max)
-else:
-    print(two + 112)
->>>>>>> refs/remotes/origin/readme
     
