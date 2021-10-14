b74aaa28811fda02f946e2b8afeb98b545157a3d
Andrew <40237958+reset54@users.noreply.github.com>
2021-10-14 11:23:52 +0300

    Update edit_string.py

--- python/sample_project/edit_string.py
+++ python/sample_project/edit_string.py
@@ -1,18 +1,7 @@
-<<<<<<< HEAD
-
 our_input_string = input().strip()
 #print(our_input_string)
 transform_str = our_input_string.split()
 transform_str.pop(0)
 transform_str.pop(len(transform_str)-1)
 for s in transform_str:
-=======
-
-our_input_string = input().strip()
-#print(our_input_string)
-transform_str = our_input_string.split()
-transform_str.pop(0)
-transform_str.pop(len(transform_str)-1)
-for s in transform_str:
->>>>>>> refs/remotes/origin/readme
     print(s, end=" " )
