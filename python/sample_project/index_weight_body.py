8a1a64e1a4a022316022ea8e634039664580d83b
Andrew <40237958+reset54@users.noreply.github.com>
2021-10-14 11:24:40 +0300

    Update index_weight_body.py

--- python/sample_project/index_weight_body.py
+++ python/sample_project/index_weight_body.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # m h
 string = input()
 divided = string.split(" ")
@@ -9,16 +8,4 @@ m = m.replace(",",".")
 
 mass = float(m)
 height = float(divided[1])
-=======
-# m h
-string = input()
-divided = string.split(" ")
-m = divided[0]
-m = m.replace(",",".")
-
-# print(m) # for test
-
-mass = float(m)
-height = float(divided[1])
->>>>>>> refs/remotes/origin/readme
 print(round(mass/height ** 2, 1))
