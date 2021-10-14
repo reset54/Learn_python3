80bfaf571afbf45802f02e67d5d143e6868540ec
Andrew <40237958+reset54@users.noreply.github.com>
2021-10-14 11:08:05 +0300

    Update calculate_cost_delivery.py

--- python/sample_project/calculate_cost_delivery.py
+++ python/sample_project/calculate_cost_delivery.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import math
 
 
@@ -8,8 +7,7 @@ try:
 
     if (weight_burden_gramms <= 500):
         cost_delivery = math.ceil(weight_burden_gramms // 100 + (weight_burden_gramms % 100)/100) * 80
-        print(int(cost_delivery))
-    
+        print(int(cost_delivery)) 
     elif (1000 >= weight_burden_gramms >= 500):
         cost_delivery = math.ceil((weight_burden_gramms // 100 + weight_burden_gramms%100 / 100)) * 70
         print(int(cost_delivery))
@@ -21,32 +19,5 @@ try:
     else:
         print("Sorry, something goes innapropriatelly, please check correctly your data input")
         exit()
-
 except ValueError:
-=======
-import math
-
-
-try:
-    string = input()
-    weight_burden_gramms = int(string)
-
-    if (weight_burden_gramms <= 500):
-        cost_delivery = math.ceil(weight_burden_gramms // 100 + (weight_burden_gramms % 100)/100) * 80
-        print(int(cost_delivery))
-    
-    elif (1000 >= weight_burden_gramms >= 500):
-        cost_delivery = math.ceil((weight_burden_gramms // 100 + weight_burden_gramms%100 / 100)) * 70
-        print(int(cost_delivery))
-    elif (10000 >= weight_burden_gramms >= 1000):
-        cost_delivery = math.ceil((weight_burden_gramms // 100 + weight_burden_gramms%100 / 100)) * 65
-        print(int(cost_delivery))   
-    elif (weight_burden_gramms > 10000):
-        print("не возим")
-    else:
-        print("Sorry, something goes innapropriatelly, please check correctly your data input")
-        exit()
-
-except ValueError:
->>>>>>> refs/remotes/origin/readme
     print("некорректный вес")
