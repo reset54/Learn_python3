6f1e82659e345b8b73a632851a16398edcf8c7f9
Andrew <40237958+reset54@users.noreply.github.com>
2021-10-14 11:02:35 +0300

    Update prime_yield.py

--- python/Generators/prime_yield.py
+++ python/Generators/prime_yield.py
@@ -1,98 +1,57 @@
-<<<<<<< HEAD
-def f():
-    return [43, 65, 32]
-
-def genf():
-    s = 7
-    for i in [43, 65, 32]:
-        yield i
-        print(s)
-        s = s * 10 + 7
-
-g = genf()
-
-print(f"Step 1 out: {next(g)}")
-print(f"Step 2 out: {next(g)}")
-print(f"Step 3 out: {next(g)}")
-
-
 # https://youtu.be/8cMMO8fks-k
 # https://youtu.be/vn6bV6BYm7w
- 
-# 1
-def fact(n):
-    pr = 1
-    a = []
-    for i in range (1, n+1):
-        pr = pr*i
-        a.append(pr)
-    return a
+from typing import Generator
 
-print(fact(15))
-print()
+Number = 10
+input_list_1 = [43, 65, 32]
 
-# 2
-def fact_gen(n):
+def gen_function(input_list):
+    s = 1
+    for i in input_list:
+        yield i
+        print(s)
+        s = s + 1
+
+
+def factorial(num: int) -> list:
     pr = 1
-    for i in range (1, n+1):
+    list_1 = []
+    for i in range (1, num+1):
+        pr = pr * i
+        list_1.append(pr)
+    return list_1
+
+
+def factorial_generate(num: int) -> Generator[int, None, None]:
+    pr = 1
+    for i in range (1, num+1):
         pr = pr * i
         yield pr
 
 
-s = fact_gen(10)
-print(f"Step 1 out: {next(s)}")
-print(f"Step 2 out: {next(s)}")
-print(f"Step 3 out: {next(s)}")
+# tests
+def test_steps_generate(gen):
+	i = 1
+	while(gen):
+		try:
+			print(f"Step generate {i} out: {next(gen)}")
+			i += 1
+		except StopIteration:
+			print("Итерации далее невозможны, iterable больше не содержит элементов\n\n")
+			break
 
 
-for i in fact(15):
-=======
-def f():
-    return [43, 65, 32]
-
-def genf():
-    s = 7
-    for i in [43, 65, 32]:
-        yield i
-        print(s)
-        s = s * 10 + 7
-
-g = genf()
-
-print(f"Step 1 out: {next(g)}")
-print(f"Step 2 out: {next(g)}")
-print(f"Step 3 out: {next(g)}")
+# run tests
+variable_1 = gen_function(input_list_1)
+test_steps_generate(variable_1)
 
 
-# https://youtu.be/8cMMO8fks-k
-# https://youtu.be/vn6bV6BYm7w
- 
-# 1
-def fact(n):
-    pr = 1
-    a = []
-    for i in range (1, n+1):
-        pr = pr*i
-        a.append(pr)
-    return a
+variable_2 = factorial_generate(Number)
+test_steps_generate(variable_2)
 
-print(fact(15))
-print()
+list_variable = [x for x in input_list_1]
+print(f"[x for x in input_list_1] = {list_variable}\n\n")
 
-# 2
-def fact_gen(n):
-    pr = 1
-    for i in range (1, n+1):
-        pr = pr * i
-        yield pr
-
-
-s = fact_gen(10)
-print(f"Step 1 out: {next(s)}")
-print(f"Step 2 out: {next(s)}")
-print(f"Step 3 out: {next(s)}")
-
-
-for i in fact(15):
->>>>>>> refs/remotes/origin/readme
+for i in factorial(10):
     print(i, end = "-,-")
+print(factorial(15), end = "\n\n")
