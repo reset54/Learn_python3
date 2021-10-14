816d7444feb4b14bd451806a187fedc16716d839
Andrew <40237958+reset54@users.noreply.github.com>
2021-10-14 11:22:14 +0300

    Update sample_numbers_algorithm_ Eratosthenes_sieve.py

--- python/sample_project/sample_numbers_algorithm_ Eratosthenes_sieve.py
+++ python/sample_project/sample_numbers_algorithm_ Eratosthenes_sieve.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # alghorithm finding sample numbers
 array = [2]                     # beginning, start number = 2
 n = 1000                        # cycle go before number = n
@@ -9,16 +8,5 @@ for i in range(3, n + 1):       # range(start, stop[, step]) -> range object
             k = 1
     if k == 0:
         array.append(i)         
-=======
-# alghorithm finding sample numbers
-array = [2]                     # beginning, start number = 2
-n = 1000                        # cycle go before number = n
-for i in range(3, n + 1):       # range(start, stop[, step]) -> range object
-    k = 0                       # 
-    for j in array:
-        if i % j == 0:
-            k = 1
-    if k == 0:
-        array.append(i)         
->>>>>>> refs/remotes/origin/readme
+
 print(array)
