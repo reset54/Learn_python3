ef62108bee5c886c8705bea4bf809d83f83a969e
Andrew <40237958+reset54@users.noreply.github.com>
2021-10-14 11:22:37 +0300

    Update reverse_array.py

--- python/sample_project/reverse_array.py
+++ python/sample_project/reverse_array.py
@@ -1,16 +1,7 @@
-<<<<<<< HEAD
 our_input_string = input()
 def reverse_string(string):
     for s in string[::-1]:
         print(s, end='')
 
 # test
-=======
-our_input_string = input()
-def reverse_string(string):
-    for s in string[::-1]:
-        print(s, end='')
-
-# test
->>>>>>> refs/remotes/origin/readme
 reverse_string(our_input_string)
