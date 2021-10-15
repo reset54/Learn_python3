ba983a697e8bced38e90ff3e77b9e893cfd9e725
Andrew <40237958+reset54@users.noreply.github.com>
2021-10-15 08:31:55 +0300

    Update test_exception.py

--- Test/test_exception.py
+++ Test/test_exception.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #
 try:
     data = [{"name": "Пётр", "phone": "123"}, {"name": "Иван"}]
@@ -17,24 +16,4 @@ except TypeError:
 except NameError:
     print("NameError")
 except Exception:
-=======
-#
-try:
-    data = [{"name": "Пётр", "phone": "123"}, {"name": "Иван"}]
-    print(data[0]["last"])
-
-except ZeroDivisionError:
-    print("ZeroDivisionError")
-except IndexError:
-    print("IndexError")
-except SyntaxError:
-    print("SyntaxError")
-except KeyError:
-    print("KeyError")
-except TypeError:
-    print("TypeError")
-except NameError:
-    print("NameError")
-except Exception:
->>>>>>> refs/remotes/origin/readme
     print("Exception")
