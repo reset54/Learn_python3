ae88180f417b6159a39e1e14fb804ba60c2d7116
Andrew <40237958+reset54@users.noreply.github.com>
2021-11-07 15:03:09 +0300

    Update sample_numbers_algorithm_ Eratosthenes_sieve.py
    statement -> in func # def alg_sieve_of_Eratosthenes(num=arg)
    addition type hint for arg_func=num(integer) and return=list (array)

--- python/sample_project/sample_numbers_algorithm_ Eratosthenes_sieve.py
+++ python/sample_project/sample_numbers_algorithm_ Eratosthenes_sieve.py
@@ -1,12 +1,15 @@
-# alghorithm finding sample numbers
-array = [2]                     # beginning, start number = 2
-n = 1000                        # cycle go before number = n
-for i in range(3, n + 1):       # range(start, stop[, step]) -> range object
-    k = 0                       # 
-    for j in array:
-        if i % j == 0:
-            k = 1
-    if k == 0:
-        array.append(i)         
+def alg_sieve_of_Eratosthenes(num: int) -> list:    # cycle go before number = num
+    """ alghorithm finding sample numbers """
+    array = [2]                                     # beginning, start number = 2                      
+    for i in range(3, num + 1):                     # range(start, stop[, step]) -> range object
+        k = 0                            
+        for j in array:
+            if i % j == 0:
+                k = 1
+        if k == 0:
+            array.append(i)         
+    return(array)
 
-print(array)
+# run func
+print(alg_sieve_of_Eratosthenes(1000))
+print(alg_sieve_of_Eratosthenes(100))
