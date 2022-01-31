23d7fd6ab5351daef062e96e8f145c5eb1d038fa
Andrew <40237958+reset54@users.noreply.github.com>
2022-01-31 15:13:06 +0300

    Update r_strings.py

--- python/RegularExpressions/r_strings.py
+++ python/RegularExpressions/r_strings.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import re
 
 pattern = r"х.й"
@@ -47,54 +46,4 @@ else:
 if re.match(pattern_2, "yrgcn"):
     print("YES Match test 4")
 else:
-=======
-import re
-
-pattern = r"х.й"
-
-def test_matching(pattern):
-    """check match with pattern"""
-    if re.match(pattern, "хей"):
-        print("YES Match test 1")
-    else:
-        print("NO Match test 1")
-    if re.match(pattern, "хай"):
-        print("YES Match test 2")
-    else:
-        print("NO Match test 2")
-    if re.match(pattern, "хирльирьтий"):
-        print("YES Match test 3")
-    else:
-        print("NO Match test 3")
-    if re.match(pattern, "хй"):
-        print("YES Match test 4")
-    else:
-        print("NO Match test 4")
-    if re.match(pattern, ""):
-        print("YES Match test 5")
-    else:
-        print("NO Match test 5")
-
-print('        "^" - symbol start string')
-print('        "$" - symbol end string') 
-
-
-pattern_2 = r"^y...o$"
-
-if re.match(pattern_2, "yello"):
-    print("YES Match test 1")
-else:
-    print("NO Match test 1")
-if re.match(pattern_2, "yahoo"):
-    print("YES Match test 2")
-else:
-    print("NO Match test 2")
-if re.match(pattern_2, "yellow"):
-    print("YES Match test 3")
-else:
-    print("NO Match test 3")
-if re.match(pattern_2, "yrgcn"):
-    print("YES Match test 4")
-else:
->>>>>>> refs/remotes/origin/readme
     print("NO Match test 4")
